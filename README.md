6239a2bb9ede67ce119e689b39a9b6886e3ca057
Heeill Wang <showx123@naver.com>
2020-12-19 17:40:46 +0900

    update : recursively find Cargo.toml

--- README.md
+++ README.md
@@ -4,5 +4,5 @@
 - `cargo install crate-web`
 
 ## Usage
-Once installed, the following command will open crate's webpage on [Crates.io](https://crates.io/). Be aware that following command should be executed on crate root.
+Once installed, the following command will open crate's webpage on [Crates.io](https://crates.io/).
 - `$ crate-web`
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,46 @@
+use std::env::current_dir;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 use toml_edit::Document;
 
+fn get_toml() -> Option<File> {
+    let mut dir = current_dir().unwrap();
+
+    loop {
+        let mut cur = dir.clone();
+
+        cur.push("Cargo.toml");
+        match File::open(cur) {
+            Ok(f) => {
+                return Some(f);
+            }
+            Err(_) => {
+                if !dir.pop() {
+                    return None;
+                }
+                continue;
+            }
+        }
+    }
+}
+
 fn main() {
-    let toml = File::open("./Cargo.toml").unwrap();
+    let toml = match get_toml() {
+        Some(toml) => toml,
+        None => {
+            println!("cannot find Cargo.toml");
+            return;
+        }
+    };
+
     let mut buf_reader = BufReader::new(toml);
     let mut contents = String::new();
     buf_reader.read_to_string(&mut contents).unwrap();
 
     let doc = contents.parse::<Document>().expect("invalid doc");
-    match doc["package"]["name"].as_str() {
-        Some(s) => {
-            let url = format!("{}{}", "https://crates.io/crates/", s);
-            open::that(url).expect("url open fails!");
-        }
-        None => {}
+    if let Some(s) = doc["package"]["name"].as_str() {
+        let url = format!("{}{}", "https://crates.io/crates/", s);
+        open::that(url).expect("url open fails!");
     }
 }
